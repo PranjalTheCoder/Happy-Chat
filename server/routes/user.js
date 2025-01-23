8bd08ddbf649de4c0abadd674d3d37f795ffdd81
PranjalTheCoder <pranjaltamrakar0794@gmail.com>
2025-01-24 04:29:35 +0530

    Updated API code

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,10 +1,14 @@
 import express from "express";
 import { 
+    acceptFriendRequest,
+    getMyFriends,
+    getMyNotifications,
     getMyProfile, 
     login, 
     logout, 
     newUser, 
-    searchUser 
+    searchUser, 
+    sendFriendRequest
 } from "../controllers/user.js";
 import { singleAvatar } from "../middlewares/multer.js";
 import { isAuthenticated } from "../middlewares/auth.js";
@@ -43,11 +47,11 @@ app.get("/me", getMyProfile);
 app.get("/logout", logout);
 app.get("/search", searchUser);
 
-// app.put("/sendrequest", sendRequestValidator(), validateHandler, sendFriendRequest);
-// app.put("/acceptrequest", acceptRequestValidator(), validateHandler, acceptFriendRequest);
+app.put("/sendrequest", sendRequestValidator(), validateHandler, sendFriendRequest);
+app.put("/acceptrequest", acceptRequestValidator(), validateHandler, acceptFriendRequest);
 
-// app.get("/notifications", getMyNotifications);
-// app.get("/friends", getMyFriends);
+app.get("/notifications", getMyNotifications);
+app.get("/friends", getMyFriends);
 
 
 
