3ef4bc4d87845c936af2aacfc30f0bd0eeda0bea
Pranjal Tamrakar <154547923+PranjalTheCoder@users.noreply.github.com>
2025-02-23 11:04:05 +0530

    Update RenderAttachment.jsx

--- client/src/components/shared/RenderAttachment.jsx
+++ client/src/components/shared/RenderAttachment.jsx
@@ -3,6 +3,7 @@ import { transformImage } from "../../lib/features";
 import { FileOpen as FileOpenIcon } from "@mui/icons-material";
 
 const RenderAttachment = (file, url) => {
+  // console.log
   switch (file) {
     case "video":
       return <video src={url} preload="none" width={"200px"} controls />;
